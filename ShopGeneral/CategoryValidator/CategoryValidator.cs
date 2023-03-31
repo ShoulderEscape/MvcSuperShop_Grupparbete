3c831d0ce833ffc1d854f781a3c3e19ae67c5c46
AxelFlyckt_StudyPC <axelflyckt00@gmail.com>
2023-03-31 10:12:03 +0200

    Adding CategoryValidator funktionality

--- ShopGeneral/CategoryValidator/CategoryValidator.cs
+++ ShopGeneral/CategoryValidator/CategoryValidator.cs
@@ -20,7 +20,32 @@ namespace ShopGeneral.CategoryValidator
                     categories.Add(category);
                 }      
             });
+            string str = "";
+            categories.ForEach(category => str += $"{category.Name}\n");
+
+            //savefile(str,getfilename());
             return categories;
+        }
+        public void savefile(string str, string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                File.Create(filename).Close();
+            }
+               
+            File.AppendAllText(filename, str);
+        }
+        public string getfilename()
+        {
+            DateTime now = DateTime.Now;
+
+            string month = "" + now.Month;
+            string day = "" + now.Day;
+            if (month.Length == 1) month = 0 + month;
+            if (day.Length == 1) day = 0 + day;
+            string filename = $".\\outfiles\\category\\missingproducts-{now.Year}{month}{day}.txt";
+            return filename;
+
         }
     }
 }
