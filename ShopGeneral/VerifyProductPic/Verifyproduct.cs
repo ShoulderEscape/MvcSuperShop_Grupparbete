23a31ce2c41fa3bdaa82657892b02bee00325eb1
izuvell1 <isaiasprio1@gmail.com>
2023-04-03 14:51:42 +0200

    allt funkar

--- ShopGeneral/VerifyProductPic/Verifyproduct.cs
+++ ShopGeneral/VerifyProductPic/Verifyproduct.cs
@@ -1,31 +1,38 @@
-﻿using System;
+﻿using ShopGeneral.Commands;
+using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using ShopGeneral.Data;
 
 class Verifyproduct
 {
-    public void ProducVerification  
+    public async void  ProductVerification (IEnumerable <ShopGeneral.Data.Product> products)
     {
         var client = new HttpClient();
 
-        Console.Write("Enter the URL of the image to fetch: ");
-        var imageUrl = Console.ReadLine();
-        Console.Write("Enter the product ID: ");
-        var productId = Console.ReadLine();
+        foreach (var product in products) 
+        {
+            try
+            {
+                var response = await client.GetAsync(product.ImageUrl);
+                response.EnsureSuccessStatusCode();
 
-        try
-        {
-            var response = await client.GetAsync(imageUrl);
-            response.EnsureSuccessStatusCode();
+                //Console.WriteLine($"HTTP error: worked flawlessly");
+                //var missingImagesDir = ".\\outfiles\\products\\";   
+                //var missingImagesPath = Path.Combine(missingImagesDir, $"missingimages-{DateTime.Now:yyyyMMdd}.txt");
+                //var errorMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: Product {product.Id} missing image: {product.ImageUrl}";
+                //File.AppendAllText(missingImagesPath, errorMessage + Environment.NewLine);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"HTTP error: {e.Message}");
+                var missingImagesDir = ".\\outfiles\\products\\";
+                var missingImagesPath = Path.Combine(missingImagesDir, $"missingimages-{DateTime.Now:yyyyMMdd}.txt");
+                var errorMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: Product {product.Id} missing image: {product.ImageUrl}";
+                File.AppendAllText(missingImagesPath, errorMessage + Environment.NewLine);
+            }
         }
-        catch (HttpRequestException e)
-        {
-            Console.WriteLine($"HTTP error: {e.Message}");
-            var missingImagesDir = ".\\outfiles\\products\\";
-            var missingImagesPath = Path.Combine(missingImagesDir, $"missingimages-{DateTime.Now:yyyyMMdd}.txt");
-            var errorMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: Product {productId} missing image: {imageUrl}";
-            File.AppendAllText(missingImagesPath, errorMessage + Environment.NewLine);
-        }
+
     }
 }
