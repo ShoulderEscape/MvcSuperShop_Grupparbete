931c1308b1e43c7ad37f1cba3aeadc5c547b2c3e
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2023-04-04 10:41:00 +0200

    Update Product.cs

--- ShopGeneral/Data/Product.cs
+++ ShopGeneral/Data/Product.cs
@@ -8,8 +8,7 @@ public class Product
     [MaxLength(50)] public string Name { get; set; }
     public Category Category { get; set; }
     public Manufacturer Manufacturer { get; set; }
-
-    public decimal discountPercentage { get; set; }
+    
     public int BasePrice { get; set; }
     public DateTime AddedUtc { get; set; }
     [MaxLength(100)]public string ImageUrl { get; set; }
