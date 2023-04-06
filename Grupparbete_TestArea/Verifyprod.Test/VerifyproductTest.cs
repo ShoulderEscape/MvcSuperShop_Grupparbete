c3d345a1e554813c139e125c55046b5f333bde18
izuvell1 <isaiasprio1@gmail.com>
2023-04-06 16:13:23 +0200

    pain

--- Grupparbete_TestArea/Verifyprod.Test/VerifyproductTest.cs
+++ Grupparbete_TestArea/Verifyprod.Test/VerifyproductTest.cs
@@ -2,36 +2,56 @@
 using Microsoft.EntityFrameworkCore;
 using ShopGeneral.Data;
 using ShopGeneral.Mailing;
-
-[TestClass]
-public class VerifyproductTest
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+namespace Grupparbete_TestArea.Verifyprod.Test 
 {
-    private ApplicationDbContext dbContext;
-
- [TestInitialize]
-    public void Initializer()
+    [TestClass]
+    public class VerifyproductTest
     {
-        var connection = new SqliteConnection("DataSource=:memory:");
-        connection.Open();
-        dbContext = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite(connection).Options);
-        dbContext.Database.EnsureCreated();
+        private ApplicationDbContext dbContext;
+        private List<string> errorlist;
+        private IVerifyproductValidator sut;
 
-
-        dbContext.AddRange(new[]
+        [TestInitialize]
+        public void Initializer()
         {
-            new Product() { AddedUtc=DateTime.Now, Id=164, BasePrice= 299, ImageUrl="ihsd0rghodrhg"},  
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            dbContext = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite(connection).Options);
+            dbContext.Database.EnsureCreated();
+
+
+            dbContext.AddRange(new[]
+            {
+            new Product() { AddedUtc=DateTime.Now, Id=164, BasePrice= 299, ImageUrl="ihsd0rghodrhg"},
             new Product() {Id=153, ImageUrl="ehweorhgorg"},
             new Product() {Id=143, ImageUrl="ohefoheohgeghs"}
         });
 
-    }
-    [TestMethod]
-    public void Call_verify_product_method()
-    {
-        var moq = new Moq.Mock<IVerifyproductValidator>();
+            errorlist = new List<string>();
+            errorlist.AddRange(new[]
+            {
+            "hello", "hiohrgorg", "rhgorhg", "ehoerhhrg", "oheohef"
+        });
 
-        moq.Object.ProductVerification(dbContext.Products);
 
-        moq.Verify(m => m.ProductVerification(dbContext.Products), Moq.Times.Once());
+        }
+        [TestMethod]
+        public void Call_verify_product_method()
+        {
+            var moq = new Moq.Mock<IVerifyproductValidator>();
+
+            moq.Object.Writetofile(errorlist);
+
+            moq.Verify(m => m.Writetofile(errorlist), Moq.Times.Once());
+        }
+
+    
+        
+
+          
+        
     }
 }
+
--- ShopGeneral/VerifyProductPic/IVerifyproductValidator.cs
+++ ShopGeneral/VerifyProductPic/IVerifyproductValidator.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 public interface IVerifyproductValidator
 {
-    public async void ProductVerification(IEnumerable<ShopGeneral.Data.Product> products)
-	{
+    public void VerifyproductMain(IEnumerable<ShopGeneral.Data.Product> products);
 
-	}
+    public async Task<IEnumerable<string>> ProductVerification(IEnumerable<ShopGeneral.Data.Product> products) => new List<string>();
+    public void Writetofile(IEnumerable<string> httperrors);
 }
